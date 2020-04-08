6c4c5edbcf5b066f830126c380892381ea52f257
PhuCuong1999 <50348385+PhuCuong1999@users.noreply.github.com>
2020-04-08 18:15:53 +0700

    Add project files.

---
+++ LearnGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn GitHub !");
+            Console.ReadKey();
+        }
+    }
+}
